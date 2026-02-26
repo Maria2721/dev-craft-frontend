f51ec28223b24139bd2f2349b7b254ff41fa3dde
Khabib1802 <79253439+Khabib1802@users.noreply.github.com>
2026-02-27 00:38:53 +0300

    Configure redux toolkit store
    * feat: add authSlice
    
    * chore: add eslint-import-resolver-typescript
    
    * feat: add store, rootReducer
    
    * feat: add useAppDispatch, useAppSelector hooks
    
    * feat: add redux Provider and PersistGate to root
    
    * refactor: reorganize project structure

---
+++ src/redux/store.ts
@@ -0,0 +1,40 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import { rootReducer } from './rootReducer';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+const persistor = persistStore(store);
+
+type RootState = ReturnType<typeof store.getState>;
+type AppDispatch = typeof store.dispatch;
+
+export { persistor, store };
+export type { AppDispatch, RootState };

d97de9195f1fad33a896d0dffcb614a135810fa8
Khabib Gadzhimagomedov <habib31.03.2002@gmail.com>
2026-02-27 20:40:35 +0300

    feat: add login, logout buttons

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,3 +1,12 @@
+import { useAppDispatch } from '../../hooks/useRedux';
+import { logout } from '../../redux/slices/authSlice';
+
 export default function DashboardPage() {
-  return <h1>Dashboard Page</h1>;
+  const dispatch = useAppDispatch();
+  return (
+    <>
+      <h1>Dashboard Page</h1>
+      <button onClick={() => dispatch(logout())}>Logout</button>
+    </>
+  );
 }

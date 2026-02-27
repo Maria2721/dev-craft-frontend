1048365aa51f0b326d4ff07b41a81e1cff9e7087
Khabib Gadzhimagomedov <habib31.03.2002@gmail.com>
2026-02-27 19:30:23 +0300

    feat: protect routes with isAuthenticated status from authSlice

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,12 +1,14 @@
 import { type ReactNode } from 'react';
 import { Navigate } from 'react-router-dom';
 
+import { useAppSelector } from '../../hooks/useRedux';
+
 interface Props {
   children: ReactNode;
 }
 
 function ProtectedRoute({ children }: Props) {
-  const isAuth = false;
+  const isAuth = useAppSelector((state) => state.auth.isAuthenticated);
   if (!isAuth) {
     return <Navigate to="/" replace />;
   }

c091750665b50689dec8f9fb8b1ebc992ceeee77
Khabib Gadzhimagomedov <habib31.03.2002@gmail.com>
2026-02-27 18:50:55 +0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+
+import DashboardPage from './pages/DashboardPage/DashboardPage';
+import HomePage from './pages/HomePage/HomePage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+
 export default function App() {
-  return <div>App is ready</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/dashboard" element={<DashboardPage />}></Route>
+        <Route path="*" element={<NotFoundPage />}></Route>
+      </Routes>
+    </div>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App.tsx';
@@ -8,10 +9,12 @@ import { persistor, store } from './redux/store.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
   </StrictMode>,
 );

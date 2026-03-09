c9743827a03c7201b7a706d92b1a77e07abd7cb4
Khabib Gadzhimagomedov <habib31.03.2002@gmail.com>
2026-03-10 01:06:51 +0300

    refactor: extract user controls into separate component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,32 +1,16 @@
-import { Link } from 'react-router-dom';
-
-import { useAppSelector } from '@/hooks/useRedux';
-
-import { ROUTES } from '@/constants';
-
 import { Container } from '../Container/Container';
 import { Navigation } from '../Navigation/Navigation';
-import { Button, Logo } from '../ui';
+import { Logo } from '../ui';
+import { UserControls } from '../UserControls/UserControls';
 import styles from './Header.module.scss';
 
 function Header() {
-  const isAuth = useAppSelector((state) => state.auth.isAuthenticated);
   return (
     <header className={styles.header}>
       <Container>
         <Logo />
         <Navigation />
-        <div>
-          {isAuth ? (
-            <Link to={ROUTES.PROFILE}>
-              <Button size="sm">Profile</Button>
-            </Link>
-          ) : (
-            <Link to={ROUTES.LOGIN}>
-              <Button size="sm">Login</Button>
-            </Link>
-          )}
-        </div>
+        <UserControls />
       </Container>
     </header>
   );

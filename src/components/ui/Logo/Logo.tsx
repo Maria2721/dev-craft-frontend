3c4632ad45f2d89610140936c9a8461ccab76e2d
Khabib Gadzhimagomedov <habib31.03.2002@gmail.com>
2026-03-15 06:31:12 +0300

    feat: update Logo component with text and variants

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -1,17 +1,21 @@
 import clsx from 'clsx';
+import type { AnchorHTMLAttributes } from 'react';
 import { Link } from 'react-router-dom';
 
 import styles from './Logo.module.scss';
 
-type LogoProps = {
-  size?: 'sm' | 'md' | 'lg';
+interface LogoProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
+  variant?: 'full' | 'icon';
   className?: string;
-};
+}
 
-export function Logo({ size = 'md', className }: LogoProps) {
+function Logo({ variant = 'full', className }: LogoProps) {
   return (
-    <Link to="/" className={clsx(styles.logo, styles[size], className)}>
-      <img src="./logo.svg" alt="Logo" />
+    <Link to="/" className={clsx(styles.logo, styles[variant], className)}>
+      <img src="/logo.svg" alt="" />
+      <span>DevCraft</span>
     </Link>
   );
 }
+
+export { Logo };

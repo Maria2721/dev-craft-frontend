a17a0ada88fa1d56679291baacb6ecdb97343dd1
Maria2721 <mashaivvas@gmail.com>
2026-03-28 15:33:35 +0300

    feat: add loading of list of topics on the map page for navigation

--- src/pages/MapPage/MapPage.tsx
+++ src/pages/MapPage/MapPage.tsx
@@ -1,7 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+
+import { useTopics } from '@/hooks/useTopics';
+
 export default function MapPage() {
+  const topics = useTopics();
+  const navigate = useNavigate();
+
+  const openTopic = (topicId: string, slug: string) => {
+    void navigate(`/topics/${slug}`, { state: { topicId } });
+  };
+
   return (
-    <>
+    <div>
       <h1>Map Page</h1>
-    </>
+
+      <ul>
+        {topics.map((topic) => (
+          <li key={topic.id}>
+            <button onClick={() => openTopic(topic.id, topic.slug)}>{topic.title}</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
